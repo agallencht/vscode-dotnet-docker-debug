453e55e025ef101f8e5162a78f785c2ff3014da3
Alberto Gallen <agallen@chumillastechnology.com>
2024-07-07 11:38:56 +0200

    Ajustes net8

--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -21,5 +23,18 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+System.Console.WriteLine( "====================================================");
+System.Console.WriteLine( "VAR_TEST  "  + Environment.GetEnvironmentVariable("VAR_TEST"));
+System.Console.WriteLine( "ASPNETCORE_ENVIRONMENT  "  + Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"));
+System.Console.WriteLine( "ASPNETCORE_HTTP_PORTS  "  + Environment.GetEnvironmentVariable("ASPNETCORE_HTTP_PORTS"));
+System.Console.WriteLine( "ASPNETCORE_HTTPS_PORTS  "  + Environment.GetEnvironmentVariable("ASPNETCORE_HTTPS_PORTS"));
+
+foreach(DictionaryEntry e in System.Environment.GetEnvironmentVariables())
+{
+    Console.WriteLine(e.Key  + ":" + e.Value);
+} 
+
+System.Console.WriteLine( "====================================================");
 
 app.Run();
